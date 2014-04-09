493c0434b0147a95c29681bb2b1f21763a224b46
startae14 <startae14@gmail.com>
2014-04-09 11:17:19 +0200

    Commit iniziale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simopost::Application.config.secret_token = '5e6690c1f2af2980567ba3bc2dd7e39324f0fde50ac8d7f984156f1805873d00e44d19839df1d44728ee915caf8a451263913284d31c7836788cb68ee3976d2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Simopost::Application.config.session_store :cookie_store, key: '_simopost_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Simopost::Application.config.session_store :active_record_store
