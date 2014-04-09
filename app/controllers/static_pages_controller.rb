02fb2d9c04305f984e5c1ac709789723013ed48f
startae14 <startae14@gmail.com>
2014-04-09 14:45:07 +0200

    Aggiunto il controller StaticPages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  # NON VENGONO USATE LE AZIONI REST
+  # POICHE' NELLE PAGINE STATICHE NON AVREBBERO
+  # SENSO
+  def home
+  end
+
+  def help
+  end
+end
